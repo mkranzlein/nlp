a568636465c11d7a955be572c41dbe8c4cbd6179
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2025-02-15 15:57:42 -0500

    Add core functionality, eval still missing

--- perceptron.py
+++ perceptron.py
@@ -12,34 +12,55 @@ feature should provide more signal about the outcome, which can be done by
 sampling with means farther from zero.
 """
 
-import torch
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-# ---------------------------------------------------------------------------- #
-#                                 Generate data                                #
-# ---------------------------------------------------------------------------- #
 
-# Positive samples
-n_pos = 100
-feat_1= torch.normal(mean=-0.8, std=.3, size=(n_pos, 1))
-feat_2 = torch.normal(mean=-.2, std=.1, size=(n_pos, 1))
-feat_3 = torch.normal(mean=.4, std=.1, size=(n_pos, 1))
+class Perceptron:
+    def __init__(self, alpha):
+        self.weights = np.random.uniform(-1, 1, (3))
+        self.bias = 1
+        self.alpha = alpha
 
-pos_samples = torch.cat((feat_1, feat_2, feat_3), dim=1)
-pos_labels = torch.ones((100, 1))
+    def train(self, x_train, y_train, epochs):
+        for _ in range(epochs):
+            for x, y in zip(x_train, y_train):
+                y_hat = self.forward(x)
+                self.weights = self.weights + self.alpha * (y - y_hat) * x
 
-# Negative samples
-n_neg = 100
-feat_1= torch.normal(mean=.5, std=.3, size=(n_pos, 1))
-feat_2 = torch.normal(mean=.1, std=.1, size=(n_pos, 1))
-feat_3 = torch.normal(mean=.4, std=.1, size=(n_pos, 1))
-neg_samples = torch.cat((feat_1, feat_2, feat_3), dim=1)
-neg_labels = torch.zeros((100, 1))
-x = torch.cat(pos_samples, neg_samples, dim=0)
-y = torch.cat(pos_labels, neg_labels, dim=0)
+    def forward(self, x):
+        x_1 =  np.dot(self.weights, x) + self.bias
+        if x_1 >= 0:
+            return 1
+        else:
+            return -1
+        
+    def eval(self):
+        raise NotImplementedError
 
-# TODOs:
-# Shuffle x and y together
-# Initialize weights
-# Forward pass
-# Training loop
-# Eval
+def main():
+    # Positive samples
+    n_positive = 1000
+    feat_1 = np.random.normal(-.8, .3, n_positive)
+    feat_2 = np.random.normal(-.2, .1, n_positive)
+    feat_3 = np.random.normal(.4, .1, n_positive)
+    pos_samples = np.stack((feat_1, feat_2, feat_3), axis=1)
+    pos_labels = np.ones((n_positive))
+
+    # Negative samples
+    n_negative = 1000
+    feat_1 = np.random.normal(.5, .3, n_negative)
+    feat_2 = np.random.normal(.1, .1, n_negative)
+    feat_3 = np.random.normal(.4, .1, n_negative)
+    neg_samples = np.stack((feat_1, feat_2, feat_3), axis=1)
+    neg_labels = np.zeros(n_positive)
+    x = np.concat((pos_samples, neg_samples), axis=0)
+    y = np.concat((pos_labels, neg_labels), axis=0)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, shuffle=True)
+
+    perceptron = Perceptron(alpha=.2)
+    perceptron.train(x_train, y_train, 2)
+    #perceptron.eval(x_test, y_test)
+
+if __name__ == "__main__":
+    main()
