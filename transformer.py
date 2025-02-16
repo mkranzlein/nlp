4dde1c6ae188ba2b12100991c6050beae623d344
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2025-02-15 21:07:10 -0500

    Add some architecture scaffholding

---
+++ transformer.py
@@ -0,0 +1,26 @@
+"""Encoder-only transformer."""
+
+class Transformer:
+    """
+    - Embeddings
+    - Positional encoding
+    - Encoder
+        - MultiheadAttention
+        - LayerNorm
+        - FeedForward
+        - LayerNorm
+    """
+    def __init__(self):
+        raise NotImplementedError
+
+    def self_attention(self):
+        raise NotImplementedError
+
+    def embedding(self):
+        raise NotImplementedError
+
+    def layer_norm(self):
+        raise NotImplementedError
+
+    def positional_encoding(self):
+        raise NotImplementedError
