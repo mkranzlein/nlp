322f5e3a614fafcd089d1a2c016309267e4a3c23
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2025-02-16 20:06:02 -0500

    Update docstrings

--- feedforward.py
+++ feedforward.py
@@ -1,3 +1,4 @@
+"""Create, train, and evaluate a feedforward neural network."""
 import torch
 from torch import nn
 
@@ -9,15 +10,19 @@ class NeuralNetwork(nn.module):
         self.weights = None
         self.bias = None
 
-    def forward(x):
+    def forward(x: torch.Tensor):
+        """Forward pass on inputs through hidden layer."""
         raise NotImplementedError
         # Set up inputs
         # Hidden layer
         # Activation
         # Output
 
-    def train(self):
+    def train(self, x_train, y_train, epochs: int):
+        """Train model for a specified number of epochs."""
         raise NotImplementedError
+        # TODO: Take slices of the input tensors as batches to avoid dataloaders
+        # for transparency? Or just use dataloaders for convenience?
 
 
 class SimpleNeuralNetwork(nn.module):
