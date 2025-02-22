5fdd48a5e267c6980d1eba881e1fdb26bd67955d
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2025-02-21 23:49:40 -0500

    Improve param names

--- src/nlp/models/feedforward_no_deps.py
+++ src/nlp/models/feedforward_no_deps.py
@@ -15,20 +15,20 @@ class FFNN:
         self.input_dim = 3
         self.hidden_dim = 5
         # Initialize weights + bias for hidden layer
-        self.w_h = None
-        self.b_h = None
+        self.w_hidden = None
+        self.b_hidden = None
 
         self.output_dim = 1
         # Initialize weights + bias for output layer
-        self.w_o = None
-        self.b_o = None
+        self.w_out = None
+        self.b_out = None
 
     def forward(self, x: list):
         # --------------------------- Hidden Layer --------------------------- #
-        z = matmul(x, self.w_h)  # Shape (1, 3) x (3, 5) -> (1, 5)
+        z = matmul(x, self.w_hidden)  # Shape (1, 3) x (3, 5) -> (1, 5)
         for i, row in enumerate(z):
             for j in range(len(row)):
-                row[j] += self.b_h[i]
+                row[j] += self.b_hidden[i]
 
         # Apply ReLU element by element
         for i, row in enumerate(z):
@@ -36,9 +36,9 @@ class FFNN:
                 z[i][j] = relu(elem)
 
         # --------------------------- Output Layer --------------------------- #
-        logits = matmul(z, self.w_o)  # Shape (1, 5) x (5, 1) -> (1, 1)
+        logits = matmul(z, self.w_out)  # Shape (1, 5) x (5, 1) -> (1, 1)
         for i, row in enumerate(logits):
             for j in range(len(row)):
-                row[j] += self.b_o[i]
+                row[j] += self.b_out[i]
         output = sigmoid(logits)
         return output
